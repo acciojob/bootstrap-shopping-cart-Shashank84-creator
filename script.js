d1ebcf2e8cf23cb94ad517c4639e9de6fc4e997a
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-04-22 19:30:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+() => { 
+  cy.get('.container').should('have.length', 1); 
+  cy.get('.row').within(() => { 
+    cy.get('.col-lg-8').should('have.length', 1); 
+    cy.get('.col-lg-4').should('have.length', 1); 
+  }); 
+}
