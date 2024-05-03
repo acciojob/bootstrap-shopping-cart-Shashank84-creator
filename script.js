61569afda1fd87b99e4ad938c4e7119e43991447
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-03 19:11:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-() => { 
-  cy.get('.block-heading').should('have.length', 1).and('have.css', 'text-align', 'center'); 
-  cy.get('.container').should('have.length', 1); 
-  cy.get('.container').find('.row').should('have.length', 1);
-  cy.get('.row').find('.col-lg-8').should('have.length', 1); 
-  cy.get('.row').find('.col-lg-4').should('have.length', 1); 
-}
+// () => { 
+//   cy.get('.block-heading').should('have.length', 1).and('have.css', 'text-align', 'center'); 
+//   cy.get('.container').should('have.length', 1); 
+//   cy.get('.container').find('.row').should('have.length', 1);
+//   cy.get('.row').find('.col-lg-8').should('have.length', 1); 
+//   cy.get('.row').find('.col-lg-4').should('have.length', 1); 
+// }
