2dc5477da96ed18e5fba1e5ce6514d57b668b3c0
Shashank sahu <144484964+Shashank84-creator@users.noreply.github.com>
2024-05-03 19:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,4 @@
 //   cy.get('.row').find('.col-lg-4').should('have.length', 1); 
 // }
 cy.get('.block-heading p').should('have.css', 'text-align', 'center');
+cy.get('img').should('have.class', 'img-fluid').and('have.length', 3);
